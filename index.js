6549167ec3f3d5785a3f4f29b4269254dd7f07b5
Krrithik <krrithikezhil30@gmail.com>
2023-11-30 16:49:54 -0800

    created profile dropdown

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const users = [
-    'Krrithik',
-    'Steve',
-    'Bill'
-]
+let tasks = [];
+
+
+/* 
+    list of status = unassigned, not-started, started, pending, complete
+    let task = {
+        assignedTo: 'Krrithik'
+        status: 
+    }
+*/

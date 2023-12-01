7aedd481f81312db09d3245f91e439055b43eb5a
Kumanan Murugesan <kumanan12@gmail.com>
2023-11-30 20:28:36 -0500

    fixed styles for nav

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+
+
 let tasks = [];
 
+let task = {
+    assignedTo: 'Krrithik',
+    status: 'unassigned',
+    name: 'Sample Task',
+    description: 'This is a sample task'
+}
+tasks.push(task);
 
+localStorage.setItem('tasks', JSON.stringify(tasks));
+localStorage.setItem('name', 'Krrithik');
 /* 
     list of status = unassigned, not-started, started, pending, complete
     let task = {
@@ -8,3 +19,15 @@ let tasks = [];
         status: 
     }
 */
+function changeUser() {
+    let name = document.getElementById('user').value;
+    localStorage.setItem('currentUser', name);
+    document.getElementById('currentUser').innerHTML = name;
+}
+
+function displayUser() {
+    let name = localStorage.getItem('currentUser');
+    document.getElementById('currentUser').innerHTML = name;
+}
+
+displayUser();
